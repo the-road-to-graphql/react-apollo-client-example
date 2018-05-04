f916518cc126b3830b72d2d83dd014f58d3568ca
Robin Wieruch <wrobin@gmx.net>
2018-05-04 16:11:49 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -1,21 +1,124 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query, Mutation } from 'react-apollo';
+
 import './App.css';
 
-class App extends Component {
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  {
+    organization(login: "the-road-to-learn-react") {
+      repositories(first: 20) {
+        edges {
+          node {
+            id
+            name
+            url
+            viewerHasStarred
+          }
+        }
+      }
+    }
+  }
+`;
+
+const STAR_REPOSITORY = gql`
+  mutation($id: ID!) {
+    addStar(input: { starrableId: $id }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
+const App = () => (
+  <Query query={GET_REPOSITORIES_OF_ORGANIZATION}>
+    {({ data: { organization }, loading }) => {
+      if (loading || !organization) {
+        return <div>Loading ...</div>;
+      }
+
+      return (
+        <Repositories repositories={organization.repositories} />
+      );
+    }}
+  </Query>
+);
+
+class Repositories extends React.Component {
+  state = {
+    selectedRepositoryIds: [],
+  };
+
+  toggleSelectRepository = (id, isSelected) => {
+    let { selectedRepositoryIds } = this.state;
+
+    selectedRepositoryIds = isSelected
+      ? selectedRepositoryIds.filter(itemId => itemId !== id)
+      : selectedRepositoryIds.concat(id);
+
+    this.setState({ selectedRepositoryIds });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <RepositoryList
+        repositories={this.props.repositories}
+        selectedRepositoryIds={this.state.selectedRepositoryIds}
+        toggleSelectRepository={this.toggleSelectRepository}
+      />
     );
   }
 }
+
+const RepositoryList = ({
+  repositories,
+  selectedRepositoryIds,
+  toggleSelectRepository,
+}) => (
+  <ul>
+    {repositories.edges.map(({ node }) => {
+      const isSelected = selectedRepositoryIds.includes(node.id);
+
+      const rowClassName = ['row'];
+
+      if (isSelected) {
+        rowClassName.push('row_selected');
+      }
+
+      return (
+        <li className={rowClassName.join(' ')} key={node.id}>
+          <Select
+            id={node.id}
+            isSelected={isSelected}
+            toggleSelectRepository={toggleSelectRepository}
+          />{' '}
+          <a href={node.url}>{node.name}</a>{' '}
+          {!node.viewerHasStarred && <Star id={node.id} />}
+        </li>
+      );
+    })}
+  </ul>
+);
+
+const Star = ({ id }) => (
+  <Mutation mutation={STAR_REPOSITORY} variables={{ id }}>
+    {starRepository => (
+      <button type="button" onClick={starRepository}>
+        Star
+      </button>
+    )}
+  </Mutation>
+);
+
+const Select = ({ id, isSelected, toggleSelectRepository }) => (
+  <button
+    type="button"
+    onClick={() => toggleSelectRepository(id, isSelected)}
+  >
+    {isSelected ? 'Unselect' : 'Select'}
+  </button>
+);
 
 export default App;
